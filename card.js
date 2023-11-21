e877d3107389d04645df54c38e5eb097da273633
Sakthy07 <151619283+Sakthy07@users.noreply.github.com>
2023-11-21 16:30:39 +0530

    q7

---
+++ card.js
@@ -0,0 +1,23 @@
+import './index.css';
+function Car(obj)
+{
+    return(
+       <><h1>Time: 0 Seconds</h1>
+       <button className='st'>Start</button> &nbsp;
+       <button className='sp'>Stop</button> &nbsp;
+       <button className='rs'>Reset</button>
+
+       <h1>Time: 3 Seconds</h1>
+       <button className='st'>Start</button> &nbsp;
+       <button className='sp'>Stop</button> &nbsp;
+       <button className='rs'>Reset</button>
+       </>
+
+      
+    
+    );
+    
+}
+
+
+export default Car;
